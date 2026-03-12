f96ca07ba8ed73c77ec0dc953ca3b24451fd6745
Ethan Arevalo <Earevalo@ucsd.edu>
2026-03-12 10:42:47 -0700

    Add class 04

---
+++ class04/class04.R
@@ -0,0 +1,10 @@
+#' ---
+#' title: "RStudio - Lab Class 4"
+#' author: "Ethan Arevalo"
+#' date: "January 15th, 2026"
+#' ---
+
+x <- 1:50
+plot(x, sin(x))
+plot(x, sin(x), typ="l", col="orange", lwd=2)
+log((10), base = 10)
